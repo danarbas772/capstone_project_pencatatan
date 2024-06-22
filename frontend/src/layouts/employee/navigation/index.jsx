a4da66407917cc854209618d3e6579689f552c68
danarbas772 <danarbas72@gmail.com>
2024-06-22 09:18:53 +0700

    copy

--- frontend/src/layouts/employee/navigation/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as EmpDashboard } from "./Dashboard";
-export { default as EmpAttedance } from "./Attendance";
-export { default as EmpPayroll } from "./Payroll";
-export { default as EmpOvertime } from "./Overtime";
-export { default as EmpLeave } from "./Leave";
-export { default as EmpProfile } from "./Profile";
