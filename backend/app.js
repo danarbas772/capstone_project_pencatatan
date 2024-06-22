4023c990667a19a6ea2ca908f47aef5d69947df3
danarbas772 <danarbas72@gmail.com>
2024-06-22 20:43:02 +0700

    aa

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://capstone-project-pencatatan.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, 
